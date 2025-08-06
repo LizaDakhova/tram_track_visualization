e2de497fa81f4a0cf8c705e8ce8c51e507c218b3
LizaDakhova <donskaya0598@gmail.com>
2025-08-06 18:05:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
+# Список файлов для выбора
+files = {
+    "Трек до Щукинской (train)": "track_to_shuk.html",
+    "Трек от Щукинской (val/test)": "track_from_shuk.html",
+}
+
+# Виджет для выбора файла
+selected_file = st.selectbox("Выберите трек:", list(files.keys()))
+
+# Чтение и отображение выбранного файла
+with open(files[selected_file], "r", encoding="utf-8") as file:
+    html_code = file.read()
+
 with open("track_from_shuk.html") as file:
   html_code = file.read()
 
-st.components.v1.html(html_code, height=800)
+st.components.v1.html(html_code, height=900, width=1600)
